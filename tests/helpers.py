d9ad91f02f2041f247992da511b25b49798c2b02
pablogarciamez <pablogarciamez@gmail.com>
2026-09-20 10:14:43 +0200

    Added helpers.py to calculate approximate derivative

---
+++ tests/helpers.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def numericGrad(f, x, eps=1e-6):
+    x = np.array(x, dtype=float)
+    g = np.zeros_like(x)
+    for idx in np.ndindex(x.shape):
+        old = x[idx]
+        x[idx] = old + eps
+        up = f(x)
+        x[idx] = old - eps
+        down = f(x)
+        x[idx] = old
+        g[idx] = (up - down) / (2 * eps)
+    return g
--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -1,5 +1,6 @@
 import numpy as np
 from autodiff import Tensor
+from helpers import numericGrad
 
 def test_tensor_product_square_matrices():
     A = Tensor(np.array([[1, 2], [3, 4]]))
@@ -19,3 +20,21 @@ def test_differnt_shape_tensors():
     C.backward()
     np.testing.assert_allclose(A.grad, [[4, 6, 8], [4, 6, 8]])
     np.testing.assert_allclose(B.grad, [[5, 5, 5, 5], [7, 7, 7, 7], [9, 9, 9, 9]])
+
+def test_matmul_gradcheck():
+    rng = np.random.default_rng(0)
+    A_data = rng.normal(size=(5, 3))
+    B_data = rng.normal(size=(3, 2))
+
+    A, B = Tensor(A_data), Tensor(B_data)
+    C = A @ B
+    C.backward()
+
+    def L_A(a):
+        return (Tensor(a) @ Tensor(B_data)).data.sum()
+
+    def L_B(b):
+        return (Tensor(A_data) @ Tensor(b)).data.sum()
+
+    np.testing.assert_allclose(A.grad, numericGrad(L_A, A_data), atol=1e-5)
+    np.testing.assert_allclose(B.grad, numericGrad(L_B, B_data), atol=1e-5)
